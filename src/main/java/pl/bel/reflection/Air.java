10eb868308346204d5c73771f04922e65588cb15
Julietta <vald.viktor44@mail.ru>
2022-05-08 23:47:16 +0300

    Try used reflection for simple example.

---
+++ src/main/java/pl/bel/reflection/Air.java
@@ -0,0 +1,22 @@
+package pl.bel.reflection;
+
+public class Air {
+    String country;
+    int radius;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
