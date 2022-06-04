dbb8666e62ae89c1daf982da0c8c0c7838e8649c
Julietta <vald.viktor44@mail.ru>
2022-06-04 23:42:34 +0300

    I'll will to make just test.

---
+++ src/main/java/TestTask/GroupedCounterAlgorithm.java
@@ -0,0 +1,20 @@
+package TestTask;
+
+import java.util.Map;
+
+public class GroupedCounterAlgorithm implements Algorithm <long [], Map<Long, Long>> {
+    @Override
+    public Algorithm<long[], Map<Long, Long>> execute(long[] data) {
+        return null;
+    }
+
+    @Override
+    public Algorithm<long[], Map<Long, Long>> sort() {
+        return null;
+    }
+
+    @Override
+    public Map<Long, Long> get() {
+        return null;
+    }
+}
